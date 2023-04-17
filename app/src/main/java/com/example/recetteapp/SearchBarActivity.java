e5b2fc2073d50c7f79206b8f650752db05ff191c
Gafrunkles <david.rossig12@gmail.com>
2023-04-17 12:04:06 +0200

    New search bar files

---
+++ app/src/main/java/com/example/recetteapp/SearchBarActivity.java
@@ -0,0 +1,15 @@
+package com.example.recetteapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchBarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_bar);
+    }
+
+}
