eb341be70e3aa5b1953163d1aaed2327aab76435
Gafrunkles <david.rossig12@gmail.com>
2023-04-30 14:39:53 +0200

    Advanced Front

--- app/src/main/java/com/example/recetteapp/AddRecette.java
+++ app/src/main/java/com/example/recetteapp/AddRecette.java
@@ -3,9 +3,16 @@ package com.example.recetteapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class AddRecette extends AppCompatActivity {
-
+    // Variable type recette
+    TextView textViewSelectType;
+    boolean[] selectedType;
+    ArrayList<Integer> langList = new ArrayList<>();
+    String[] langArray = {"Petit-dej", "Plat", "Apero", "C", "Python", "Javascript"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
