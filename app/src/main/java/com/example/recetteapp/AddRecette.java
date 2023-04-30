971e9ef64cfe2714a6cf0608eef5c971053271a8
Gafrunkles <david.rossig12@gmail.com>
2023-04-30 17:38:49 +0200

    Advancede front

--- app/src/main/java/com/example/recetteapp/AddRecette.java
+++ app/src/main/java/com/example/recetteapp/AddRecette.java
@@ -3,19 +3,23 @@ package com.example.recetteapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class AddRecette extends AppCompatActivity {
-    // Variable type recette
-    TextView textViewSelectType;
-    boolean[] selectedType;
-    ArrayList<Integer> langList = new ArrayList<>();
-    String[] langArray = {"Petit-dej", "Plat", "Apero", "C", "Python", "Javascript"};
+    Spinner spinnerType;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_recette);
+        //DropDown des types
+        spinnerType = findViewById(R.id.spinnerType);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.types, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerType.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/example/recetteapp/Options.java
+++ app/src/main/java/com/example/recetteapp/Options.java
@@ -7,16 +7,23 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.Switch;
 
 public class Options extends AppCompatActivity {
-
+    Spinner spinnerLangues;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_options);
 
         Switch darkMode = findViewById(R.id.switchdarkmode);
+        //DropDown des types
+        spinnerLangues = findViewById(R.id.spinnerLangues);
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.langues, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinnerLangues.setAdapter(adapter);
         //Mettre en dark Mode background
         darkMode.setOnClickListener(new View.OnClickListener() {
             @Override
